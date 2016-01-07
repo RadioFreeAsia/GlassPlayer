917af1b312f2987d21c56eeb64f85d4c93ea5127
Fred Gleason <fredg@paravelsystems.com>
2016-01-07 14:20:54 -0500

    2016-01-07 Fred Gleason <fredg@paravelsystems.com> 	* Added a 'Codec' class in 'src/common/codec.cpp' and 	'src/common/codec.h'.

---
+++ src/common/codec.cpp
@@ -0,0 +1,215 @@
+// codec.cpp
+//
+// Abstract base class for audio codecs.
+//
+//   (C) Copyright 2014-2016 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include "codec.h"
+#include "logging.h"
+
+Codec::Codec(Codec::Type type,Ringbuffer *ring,QObject *parent)
+{
+  codec_ring1=ring;
+  codec_bitrate=128;
+  codec_channels=2;
+  codec_quality=0.5;
+  codec_source_samplerate=48000;
+  codec_stream_samplerate=48000;
+  codec_ring2=NULL;
+}
+
+
+Codec::~Codec()
+{
+  if(codec_src_state!=NULL) {
+    src_delete(codec_src_state);
+  }
+  if(codec_src_data!=NULL) {
+    delete codec_src_data;
+  }
+  if((codec_ring2!=codec_ring1)&&(codec_ring2!=NULL)) {
+    delete codec_ring2;
+  }
+}
+
+
+unsigned Codec::bitrate() const
+{
+  return codec_bitrate;
+}
+
+
+void Codec::setBitrate(unsigned rate)
+{
+  codec_bitrate=rate;
+}
+
+
+unsigned Codec::channels() const
+{
+  return codec_channels;
+}
+
+
+void Codec::setChannels(unsigned chans)
+{
+  codec_channels=chans;
+}
+
+
+double Codec::quality() const
+{
+  return codec_quality;
+}
+
+
+void Codec::setQuality(double qual)
+{
+  codec_quality=qual;
+}
+
+
+unsigned Codec::sourceSamplerate() const
+{
+  return codec_source_samplerate;
+}
+
+
+void Codec::setSourceSamplerate(unsigned rate)
+{
+  codec_source_samplerate=rate;
+}
+
+
+unsigned Codec::streamSamplerate() const
+{
+  return codec_stream_samplerate;
+}
+
+
+void Codec::setStreamSamplerate(unsigned rate)
+{
+  codec_stream_samplerate=rate;
+}
+
+
+bool Codec::start()
+{
+  int err;
+
+  if(codec_source_samplerate==codec_stream_samplerate) {
+    codec_pcm_buffer[0]=new float[MAX_AUDIO_CHANNELS*MAX_AUDIO_BUFFER];
+    codec_pcm_buffer[1]=NULL;
+    codec_pcm_in=codec_pcm_buffer[0];
+    codec_pcm_out=codec_pcm_buffer[0];
+    codec_src_state=NULL;
+    codec_src_data=NULL;
+    codec_ring2=codec_ring1;
+  }
+  else {
+    codec_pcm_buffer[0]=new float[MAX_AUDIO_CHANNELS*MAX_AUDIO_BUFFER];
+    codec_pcm_buffer[1]=new float[MAX_AUDIO_CHANNELS*MAX_AUDIO_BUFFER*6];
+    codec_pcm_in=codec_pcm_buffer[0];
+    codec_pcm_out=codec_pcm_buffer[1];
+    if((codec_src_state=src_new(SRC_SINC_FASTEST,codec_channels,&err))==NULL) {
+      Log(LOG_ERR,"unable to create sample rate converter");
+      return false;
+    }
+    codec_src_data=new SRC_DATA;
+    memset(codec_src_data,0,sizeof(SRC_DATA));
+    codec_src_data->data_in=codec_pcm_buffer[0];
+    codec_src_data->data_out=codec_pcm_buffer[1];
+    codec_src_data->output_frames=MAX_AUDIO_BUFFER*6;
+    codec_src_data->src_ratio=
+      (double)codec_stream_samplerate/(double)codec_source_samplerate;
+    codec_ring2=new Ringbuffer(262144,codec_channels);
+  }
+  return startCodec();
+}
+
+
+QString Codec::codecTypeText(Codec::Type type)
+{
+  QString ret=tr("Unknown");
+
+  switch(type) {
+  case Codec::TypeAac:
+    ret=tr("MPEG-2 AAC Main Profile");
+    break;
+
+  case Codec::TypeFdk:
+    ret=tr("MPEG-4 AAC High Efficiency");
+    break;
+
+  case Codec::TypeMpegL2:
+    ret=tr("MPEG-1 Layer 2");
+    break;
+ 
+  case Codec::TypeMpegL3:
+    ret=tr("MPEG-1 Layer 3");
+    break;
+ 
+  case Codec::TypeOpus:
+    ret=tr("Opus [RFC 6716]");
+    break;
+
+  case Codec::TypePcm16:
+    ret=tr("PCM16 Uncompressed");
+    break;
+
+  case Codec::TypeVorbis:
+    ret=tr("Ogg Vorbis");
+    break;
+
+  case Codec::TypeLast:
+    break;
+  }
+
+  return ret;
+}
+
+
+void Codec::decode(Connector *conn)
+{
+  /*
+  int n;
+  int err=0;
+
+  if(codec_src_state!=NULL) {
+    while(codec_ring1->readSpace()>=pcmFrames()) {
+      n=codec_ring1->read(codec_pcm_in,pcmFrames());
+      codec_src_data->input_frames=n;
+      if((err=src_process(codec_src_state,codec_src_data))!=0) {
+	Log(LOG_WARNING,QString().sprintf("SRC error [%s]",src_strerror(err)));
+	continue;
+      }
+      n=codec_src_data->output_frames_gen;
+      codec_ring2->write(codec_pcm_out,n);
+    }
+  }
+  while(codec_ring2->readSpace()>=pcmFrames()) {
+    n=codec_ring2->read(codec_pcm_in,pcmFrames());
+    encodeData(conn,codec_pcm_in,n);
+  }
+  */
+}
+
+
+Ringbuffer *Codec::ring()
+{
+  return codec_ring1;
+}
---
+++ src/common/codec.h
@@ -0,0 +1,89 @@
+// codec.h
+//
+// Abstract base class for audio codecs.
+//
+//   (C) Copyright 2014-2016 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef CODEC_H
+#define CODEC_H
+
+#include <queue>
+#include <vector>
+
+#include <dlfcn.h>
+#include <syslog.h>
+
+#include <samplerate.h>
+
+#include <QObject>
+
+#include "connector.h"
+#include "glasslimits.h"
+#include "ringbuffer.h"
+
+#define MAX_AUDIO_BUFFER 4096
+
+class Codec : public QObject
+{
+  Q_OBJECT;
+ public:
+  enum Type {TypeAac=0,TypeFdk=1,TypeMpegL2=2,TypeMpegL3=3,
+	     TypeVorbis=4,TypeOpus=7,TypePcm16=5,TypeLast=6};
+  Codec(Codec::Type type,Ringbuffer *ring,QObject *parent=0);
+  ~Codec();
+  unsigned bitrate() const;
+  void setBitrate(unsigned rate);
+  unsigned channels() const;
+  void setChannels(unsigned chans);
+  double quality() const;
+  void setQuality(double qual);
+  unsigned sourceSamplerate() const;
+  void setSourceSamplerate(unsigned rate);
+  unsigned streamSamplerate() const;
+  void setStreamSamplerate(unsigned rate);
+  virtual bool isAvailable() const=0;
+  virtual QString contentType() const=0;
+  virtual QString defaultExtension() const=0;
+  virtual QString formatIdentifier() const=0;
+  virtual bool start();
+  static QString codecTypeText(Codec::Type type);
+
+ public slots:
+  virtual void decode(Connector *conn);
+
+ protected:
+  virtual void decodeData(Connector *conn,const float *pcm,int len)=0;
+  virtual bool startCodec()=0;
+  Ringbuffer *ring();
+
+ private:
+  Ringbuffer *codec_ring1;
+  Ringbuffer *codec_ring2;
+  unsigned codec_bitrate;
+  unsigned codec_channels;
+  double codec_quality;
+  unsigned codec_source_samplerate;
+  unsigned codec_stream_samplerate;
+  SRC_STATE *codec_src_state;
+  SRC_DATA *codec_src_data;
+  float *codec_pcm_in;
+  float *codec_pcm_out;
+  float *codec_pcm_buffer[2];
+};
+
+
+#endif  // CODEC_H
